939c3307250daa5a14ebd2f17163702128b58750
Unknown <samnadeauvachon@gmail.com>
2018-03-22 18:50:24 -0400

    Implémentation de méthodes Partie
    Ajouts de l'affichage des règlements et des joueurs

--- pydomino/partie.py
+++ pydomino/partie.py
@@ -60,29 +60,43 @@ class Partie:
         """
 
         plateau = pydomino.Plateau()
-
         donnes = distribuer_dominos(nombre_joueurs)
-
         partie = pydomino.Partie(plateau, donnes)
-
         return partie
 
     @staticmethod
     def afficher_instructions():
         """
         Méthode statique qui affiche les instructions du jeu
-
         """
-        # TODO: À compléter
-        pass
+
+        print("\nInstructions:\n\n\
+        Le jeu peut être joué par 2, 3 ou 4 joueurs. À partir d’un ensemble de 28 dominos ”doublesix”,\n\
+        chaque joueur a une donne de dominos constituée au hasard (son jeu). Pour une\n\
+        partie à 2 joueurs, chaque joueur reçoit 7 dominos. Pour une partie à 3 ou 4 joueurs, chaque\n\
+        joueur reçoit 6 dominos.\n\n\
+        Le premier joueur à déposer un domino est celui qui a le domino le plus élevé (les trois\n\
+        dominos les plus élevés sont [6,6], [5,6] et [5,5]). Il doit nécessairement déposer ce domino en premier.\n\
+        Ensuite, chaque joueur joue à tour de rôle. Pour déposer un domino, un joueur doit choisir un domino de\n\
+        sa donne qui a un numéro identique à une des extrémités de la suite de dominos qui s’assemble sur la table.\n\
+        Par exemple, si la suite sur la table est la suivante : [3,6][6,6][6,5], le joueur dont c’est le tour peut\n\
+        jouer un domino contenant soit le numéro 3 (par exemple, [2,3]), soit le numéro 5 (par exemple [5,5]). \n\
+        Si un joueur ne peut jouer aucun domino, il doit passer son tour.\n\n\
+        La partie se termine dans deux conditions :\n\
+        \t1. Si un joueur a déposé tous ses dominos et a vidé sa donne. Ce joueur est déclaré gagnant.\n\
+        \t2. Si aucun joueur ne peut déposer un domino (ils auront donc tous passé leur tour), la partie\n\
+        \t   est arrêtée. Le joueur à qui il reste le moins de dominos dans sa donne est déclaré gagnant.\n\
+        \t   En comptant le nombre de dominos restants, il se peut qu’il y ait égalité entre 2 joueurs ou plus.")
 
     def afficher_etat_donnes(self):
         """
         Méthode qui affiche l'état des donnes des joueurs de la partie.
         L'information affichée doit contenir le numéro du joueur et le nombre de dominos de sa donne.
         """
-        # TODO: À compléter
-        pass
+
+        for i in range(len(self.donnes)):
+            print("\nJoueur " + str(i + 1))
+            print('Nombre de dominos: ' + str(len(self.donnes[i])))
 
     def trouver_premier_joueur(self):
         """
@@ -258,4 +272,4 @@ class Partie:
 
 
 if __name__ == '__main__':
-    distribuer_dominos(4)
+    Partie.nouvelle_partie(2).afficher_etat_donnes()
